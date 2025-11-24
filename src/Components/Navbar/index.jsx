0bc8ed6e7543b61ea3e01a9f2fcff40302610a89
Faimel Mamahit <s22310276@student.unklab.ac.id>
2025-11-24 18:59:50 +0800

    first commit after fixing branch issue

---
+++ src/Components/Navbar/index.jsx
@@ -0,0 +1,98 @@
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
+import logo from "../../assets/logo.png";
+import { navItems } from "../../Constants";
+
+const Navbar = () => {
+  const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setMobileDrawerOpen(!mobileDrawerOpen);
+  };
+
+  // Fungsi scroll ke section
+  const handleScroll = (id) => {
+    const section = document.getElementById(id);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+      setMobileDrawerOpen(false); // Tutup drawer mobile setelah klik
+    }
+  };
+
+  return (
+    <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
+      <div className="container px-4 mx-auto relative lg:text-sm">
+        <div className="flex justify-between items-center">
+          <div className="flex items-center flex-shrink-0">
+            <img className="h-10 w-10 mr-2" src={logo} alt="Logo" />
+            <span className="text-xl tracking-tight">VisualPlay</span>
+          </div>
+
+          {/* DESKTOP NAV */}
+          <ul className="hidden lg:flex ml-14 space-x-12">
+            {navItems.map((item, index) => (
+              <li key={index}>
+                <button
+                  onClick={() => handleScroll(item.href)}
+                  className="hover:text-pink-400"
+                >
+                  {item.label}
+                </button>
+              </li>
+            ))}
+          </ul>
+
+          <div className="hidden lg:flex justify-center space-x-12 items-center">
+            <a href="#" className="py-2 px-3 border rounded-md">
+              Sign In
+            </a>
+            <a
+              href="#"
+              className="bg-gradient-to-r from-pink-500 to-rose-500 py-2 px-3 rounded-md"
+            >
+              Create an account
+            </a>
+          </div>
+
+          {/* MOBILE MENU BUTTON */}
+          <div className="lg:hidden md:flex flex-col justify-end">
+            <button onClick={toggleNavbar}>
+              {mobileDrawerOpen ? <X /> : <Menu />}
+            </button>
+          </div>
+        </div>
+
+        {/* MOBILE NAV */}
+        {mobileDrawerOpen && (
+          <div className="fixed right-0 z-20 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center lg:hidden">
+            <ul>
+              {navItems.map((item, index) => (
+                <li key={index} className="py-4">
+                  <button
+                    onClick={() => handleScroll(item.href)}
+                    className="hover:text-orange-400"
+                  >
+                    {item.label}
+                  </button>
+                </li>
+              ))}
+            </ul>
+            <div className="flex space-x-6">
+              <a href="#" className="py-2 px-3 border rounded-md">
+                Sign In
+              </a>
+              <a
+                href="#"
+                className="py-2 px-3 rounded-md bg-gradient-to-r from-orange-500 to-orange-800"
+              >
+                Create an account
+              </a>
+            </div>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
